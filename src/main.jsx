8e9da88e5ae503e9d31921a1456673a303652bd7
mmuranaka <muranaka.matthew@gmail.com>
2025-10-06 20:50:38 -1000

    Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/next"
 
 // styles
 import './assets/styles/App.css';
